8ee18f6fa7f544a466104b13f43901873a897fc3
Beniamino Green <ben@greendalba.com>
2022-10-05 14:10:53 -0400

    😲➕🚒 Automatically Generates Anchor Points

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -2,10 +2,11 @@ use extendr_api::prelude::*;
 // use ordered_float::OrderedFloat;
 // use extendr_api::wrapper::Doubles;
 use indicatif::ProgressBar;
+use ndarray::Array2;
 use rand::thread_rng;
 use rand::Rng;
-use ndarray::{Array2};
 use rayon::prelude::*;
+use frank::*;
 
 fn sub_evaluation(assignment: &[bool], outcome: &[f64]) -> f64 {
     assignment
@@ -42,7 +43,15 @@ struct Population {
 }
 
 impl Population {
-    fn new(po_1_t: Vec<f64>, po_1_c: Vec<f64>, po_2_t: Vec<f64>, po_2_c: Vec<f64>, n_treat: usize, temperature_decay:f64, gen_size : i32) -> Population {
+    fn new(
+        po_1_t: Vec<f64>,
+        po_1_c: Vec<f64>,
+        po_2_t: Vec<f64>,
+        po_2_c: Vec<f64>,
+        n_treat: usize,
+        temperature_decay: f64,
+        gen_size: i32,
+    ) -> Population {
         let mut pop = Population {
             policies: Vec::new(),
             n_treat: n_treat,
@@ -57,17 +66,62 @@ impl Population {
             gen_size: gen_size,
         };
 
+        pop.add_anchor_points();
+
         for _ in 0..pop.gen_size {
             pop.policies.push(Policy::new(pop.n, pop.n_treat));
         }
         pop
     }
 
-    fn evaluate(&mut self) {
-        self.policies
-            .par_iter_mut()
-            .for_each(|policy| policy.evaluate(&self.po_1_t, &self.po_1_c, &self.po_2_t, &self.po_2_c));
+    fn add_anchor_points(&mut self) {
+        let treatment_adv_1 : Vec<f64> = self
+            .po_1_t
+            .iter()
+            .zip(self.po_1_c.iter())
+            .map(|(a, b)| a - b)
+            .collect();
 
+        let treatment_adv_2  : Vec<f64> = self
+            .po_2_t
+            .iter()
+            .zip(self.po_2_c.iter())
+            .map(|(a, b)| a - b)
+            .collect();
+
+        let rank_adv_1 : Vec<usize> = treatment_adv_1.rank();
+        let rank_adv_2 : Vec<usize> = treatment_adv_2.rank();
+
+        let treated_1 : Vec<bool> = rank_adv_1.iter().map(|x| x < &self.n_treat).collect();
+        let treated_2 : Vec<bool> = rank_adv_2.iter().map(|x| x < &self.n_treat).collect();
+
+        let policy_1 = Policy {
+            assignment: treated_1,
+            n: self.n,
+            n_treat: self.n_treat,
+            utility_1: None,
+            utility_2: None,
+            rank: None,
+        };
+
+        let policy_2 = Policy {
+            assignment: treated_2,
+            n: self.n,
+            n_treat: self.n_treat,
+            utility_1: None,
+            utility_2: None,
+            rank: None,
+        };
+
+        self.policies.push(policy_1);
+        self.policies.push(policy_2);
+
+    }
+
+    fn evaluate(&mut self) {
+        self.policies.par_iter_mut().for_each(|policy| {
+            policy.evaluate(&self.po_1_t, &self.po_1_c, &self.po_2_t, &self.po_2_c)
+        });
 
         self.policies.sort_by(|a, b| {
             b.utility_1
@@ -79,8 +133,8 @@ impl Population {
         let mut rank = 0;
         let mut n_seen = 1;
 
-        let mut best_utility_2 = 0.0;
-        while self.policies.iter().any(|x| x.rank.is_none()) && (self.n / n_seen) > 4  {
+        // let mut best_utility_2 = 0.0;
+        while self.policies.iter().any(|x| x.rank.is_none()) && (self.n / n_seen) > 4 {
             rank += 1;
             n_seen += 1;
             let mut current_best_y = 0.0;
@@ -94,7 +148,7 @@ impl Population {
             }
         }
 
-        self.policies.sort_by(|a, b| {
+        self.policies.sort_by(|b, a| {
             b.utility_2
                 .unwrap()
                 .partial_cmp(&a.utility_2.unwrap())
@@ -130,7 +184,7 @@ impl Population {
             }
         }
 
-        let mut kids : Vec<Policy> = (0..((self.gen_size - (next_gen.len() as i32)) as usize))
+        let mut kids: Vec<Policy> = (0..((self.gen_size - (next_gen.len() as i32)) as usize))
             .into_par_iter()
             .map(|_| self.create_1_kid(num_mutates))
             .collect();
@@ -139,7 +193,7 @@ impl Population {
         self.policies = next_gen;
     }
 
-    fn create_1_kid(&self, num_mutates : i32) -> Policy{
+    fn create_1_kid(&self, num_mutates: i32) -> Policy {
         let mut rng = thread_rng();
         let i1 = rng.gen_range(0..self.gen_size) as usize;
         let i2 = rng.gen_range(0..self.gen_size) as usize;
@@ -240,10 +294,16 @@ impl Policy {
         po_2_c: &Vec<f64>,
     ) {
         let mut utility_1 = sub_evaluation(&self.assignment, po_1_t);
-        utility_1 += sub_evaluation(&self.assignment.iter().map(|x| !x).collect::<Vec<bool>>(), po_1_c);
+        utility_1 += sub_evaluation(
+            &self.assignment.iter().map(|x| !x).collect::<Vec<bool>>(),
+            po_1_c,
+        );
 
         let mut utility_2 = sub_evaluation(&self.assignment, po_2_t);
-        utility_2 += sub_evaluation(&self.assignment.iter().map(|x| !x).collect::<Vec<bool>>(), po_2_c);
+        utility_2 += sub_evaluation(
+            &self.assignment.iter().map(|x| !x).collect::<Vec<bool>>(),
+            po_2_c,
+        );
 
         self.utility_1 = Some(utility_1);
         self.utility_2 = Some(utility_2);
@@ -251,8 +311,16 @@ impl Policy {
 }
 
 #[extendr]
-fn gen_opt(po_1_t: &[f64], po_1_c: &[f64], po_2_t: &[f64], po_2_c: &[f64], n_treat: i32, n_iter: u64,temperature_decay : f64, gen_size : i32) -> Result<Robj> {
-
+fn gen_opt(
+    po_1_t: &[f64],
+    po_1_c: &[f64],
+    po_2_t: &[f64],
+    po_2_c: &[f64],
+    n_treat: i32,
+    n_iter: u64,
+    temperature_decay: f64,
+    gen_size: i32,
+) -> Result<Robj> {
     let mut pop = Population::new(
         po_1_t.to_vec(),
         po_1_c.to_vec(),
@@ -260,7 +328,7 @@ fn gen_opt(po_1_t: &[f64], po_1_c: &[f64], po_2_t: &[f64], po_2_c: &[f64], n_tre
         po_2_c.to_vec(),
         n_treat as usize,
         temperature_decay,
-        gen_size
+        gen_size,
     );
 
     let bar = ProgressBar::new(n_iter);
@@ -275,27 +343,26 @@ fn gen_opt(po_1_t: &[f64], po_1_c: &[f64], po_2_t: &[f64], po_2_c: &[f64], n_tre
     let mut cols = 0;
     for policy in &pop.policies {
         if policy.rank == Some(1) {
-            cols +=1
+            cols += 1
         }
     }
 
     let mut i = 0;
-    let mut out_arr : Array2<bool> = Array2::from_elem((pop.policies[0].assignment.len(), cols-1),false);
+    let mut out_arr: Array2<bool> =
+        Array2::from_elem((pop.policies[0].assignment.len(), cols - 1), false);
     for mut col in out_arr.axis_iter_mut(Axis(1)) {
         loop {
             i += 1;
             if pop.policies[i].rank == Some(1) {
-                 col.assign(&Array::from_vec(pop.policies[i].assignment.clone()));
-                 break
+                col.assign(&Array::from_vec(pop.policies[i].assignment.clone()));
+                break;
             }
         }
     }
 
-
     let array = Robj::try_from(&out_arr).unwrap();
 
     Ok(array)
-
 }
 
 // Macro to generate exports.
